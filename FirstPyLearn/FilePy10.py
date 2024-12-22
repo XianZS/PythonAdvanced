16dbfc8be90c5429a78651fcbfee76b288f33291
XianZS <XianZhiSen_yang@outlook.com>
2024-12-22 15:57:07 +0800

    XianZS

---
+++ FirstPyLearn/FilePy10.py
@@ -0,0 +1,50 @@
+# -*- coding: UTF-8 -*-
+"""
+    @Project : PythonAdvanced 
+    @File    : FilePy10.py
+    @IDE     : PyCharm 
+    @Author  : XianZS
+    @Date    : 2024/12/22 15:01 
+    @NowThing: 工厂模式 --- 工厂函数
+"""
+"""
+    增加模块化的思想
+    防止违反 “开放封闭原则”
+    虽然减少了代码的耦合性，但是也增加了代码的抽象性
+    工厂方法模式的特点：
+        * 定义一个接口来创建对象，但工厂本身并不负责创建动作，而是由其子类决定实例化哪些类
+        * 工厂方法的创建是通过继承而不是通过实例化来完成的
+        * 工厂方法使设计更具有定制性。可以返回相同的实例或子类
+"""
+
+from abc import ABCMeta, abstractmethod
+
+
+# 在 python 中接口类的命名方式一般以 I 开头
+class IPerson(metaclass=ABCMeta):
+    @abstractmethod
+    def person_method(self):
+        pass
+
+
+class Student(IPerson):
+    def __init__(self, name):
+        self.name = name
+
+    def person_method(self):
+        print("I am a student")
+
+
+class Teacher(IPerson):
+    def __init__(self, name):
+        self.name = name
+
+    def person_method(self):
+        print("I am a teacher")
+
+
+if __name__ == '__main__':
+    s1 = Student("jom")
+    s1.person_method()
+    t1 = Teacher("kom")
+    t1.person_method()
--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# 这是一个示例 Python 脚本。
+from abc import ABCMeta, abstractmethod,ABC
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+class IPerson(metaclass=ABCMeta):
+    @abstractmethod
+    def say_something(self,some):
+        """ say hello """
+        pass
 
+class S1(IPerson):
+    def say_something(self,some):
+        print(some)
 
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
-
-
-# 按装订区域中的绿色按钮以运行脚本。
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+s1=S1()
+s1.say_something("hello")
